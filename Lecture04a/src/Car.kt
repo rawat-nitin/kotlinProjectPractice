c8244f9ee961c1731e5a140c97e94d3e6cff39cd
Milan Košťák <milankostak@seznam.cz>
2021-06-27 12:35:35 +0200

    Lecture 04a - Kotlin III - minor improvements

--- Lecture04a/src/Car.kt
+++ Lecture04a/src/Car.kt
@@ -1,5 +1,7 @@
-data class Car(val brand: String,
-               val color: String,
-               val year: Int,
-               var odometer: Double = 0.0, // default is a new car
-               val electric: Boolean = false)
+data class Car(
+    val brand: String,
+    val color: String,
+    val year: Int,
+    var odometer: Double = 0.0, // default is a new car
+    val electric: Boolean = false
+)
