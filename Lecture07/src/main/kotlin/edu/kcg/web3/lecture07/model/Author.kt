1851ad04b65112e9d346e7daa3ab679405263450
rawat-nitin <68001057+rawat-nitin@users.noreply.github.com>
2021-10-26 18:00:01 +0900

    Delete Lecture07 directory

--- Lecture07/src/main/kotlin/edu/kcg/web3/lecture07/model/Author.kt
+++
@@ -1,8 +0,0 @@
-package edu.kcg.web3.lecture07.model
-
-data class Author(
-    val firstName: String = "",
-    val lastName: String = "",
-    val books: MutableList<Book> = mutableListOf(),
-    var id: Int = -1
-)
--- Lecture07/src/main/kotlin/edu/kcg/web3/lecture07/model/Person.kt
+++
@@ -1,8 +0,0 @@
-package edu.kcg.web3.lecture07.model
-
-data class Person(
-    val name: String = "",
-    var age: Int = 0,
-    var favouriteLanguage: String = "",
-    var id: Int = -1
-)
