7af602f336253ec2fec491f8199c09f2066b0964
rawat-nitin <68001057+rawat-nitin@users.noreply.github.com>
2021-10-26 17:59:40 +0900

    Delete Lecture05 directory

--- Lecture05/src/main/kotlin/edu/kcg/web3/spring_boot_demo/model/Person.kt
+++
@@ -1,8 +0,0 @@
-package edu.kcg.web3.spring_boot_demo.model
-
-data class Person(
-    val name: String = "",
-    var age: Int = 0,
-    var favouriteLanguage: String = "",
-    var id: Int = -1
-)
