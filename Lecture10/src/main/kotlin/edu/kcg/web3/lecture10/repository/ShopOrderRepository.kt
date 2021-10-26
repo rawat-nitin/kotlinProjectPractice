47bb4e1f5a3efd3206a9df5368eb43f90e1e0440
rawat-nitin <68001057+rawat-nitin@users.noreply.github.com>
2021-10-26 18:00:32 +0900

    Delete Lecture10 directory

--- Lecture10/src/main/kotlin/edu/kcg/web3/lecture10/repository/ShopOrderRepository.kt
+++
@@ -1,8 +0,0 @@
-package edu.kcg.web3.lecture10.repository
-
-import edu.kcg.web3.lecture10.entity.ShopOrder
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface ShopOrderRepository : JpaRepository<ShopOrder, Long?>
